bfb13cdbdaed568f4658ec9f9299b998c51c4b52
bihoi <32343089+bihoi@users.noreply.github.com>
2018-03-25 13:13:00 +0100

    Changed resolutions structure

--- resolution/models.py
+++ resolution/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class YourResolutions(models.Model):
-    health = models.CharField(max_length=100)
-    career = models.CharField(max_length=100)
-    travel = models.CharField(max_length=100)
+    resolution_title = models.CharField(max_length=100)
+    resolution_category = models.CharField(max_length=100)
+    resolution_year = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.health + ' - ' + self.career +' - '+ self.travel
+        return self.resolution_title + ' - ' + self.resolution_category +' - '+ self.resolution_year
 
 class notes(models.Model):
     yourresolution = models.ForeignKey(YourResolutions, on_delete=models.CASCADE)
